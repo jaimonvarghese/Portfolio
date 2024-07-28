52c7f082578c4644e0cd4122402cca3c4e47c299
Jaimon Varghese <jaimonvarghese1224@gmail.com>
2024-07-28 09:59:44 +0530

    Edit js and css files

--- script.js
+++ script.js
@@ -29,30 +29,3 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () =>{
-	sections.forEach(sec => {
-		let top = window.scrollY;
-		let offset = sec.offsetTop - 150;
-		let height = sec.offsetHeight;
-		let id = sec.getAttribute('id');
-		
-		if(top >= offset && top < offset + height){
-			navLinks.forEach(links =>{
-				links.classList.remove('active');
-				document.querySelector('header nav a[href*='+ id +']').classList.add('active');
-
-			});
-		};
-	});
-
-	let header = document.querySelector('header');
-	
-	header.classList.toggle('sticky',window.scrollY > 100);
-
-	menuIcon.classList.remove('fa-x');
-	navbar.classList.remove('active');
-};
